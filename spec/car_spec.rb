b68c0f48287c87a24a72254a763ca88509b42f34
Michael Reeves <mreeves68@yahoo.com>
2015-04-01 14:48:30 -0600

    Refactored tests to include shared examples for Car and Truck

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -1,48 +1,9 @@
 require "car"
+require "shared_examples/a_standard_vehicle"
 
 RSpec.describe Car do
 
-	describe "attributes" do
-
-		# do not need subject explicitly-defined
-		# if we use a classname after describe
-		#subject { Car.new }
-
-		# use "subject" instead of "let"
-		# if variable is subject of example
-		#let(:car) { Car.new }
-
-		# "let" is better than "before" for
-		# setting up instance variables
-		# before(:example) do
-		#   @car = Car.new
-		# end
-
-		it "allows reading and writing for :make" do
-			pending("debugging a problem")
-			subject.make = 'Tes'
-			expect(subject.make).to eq('Test')
-		end
-
-		it "allows reading and writing for :year" do
-			subject.year = 9999
-			expect(subject.year).to eq(9999)
-			#car.year.should eq(9999) 	# deprecated syntax
-		end
-
-		it "allows reading and writing for :color" do
-			subject.color = 'foo'
-			expect(subject.color).to eq('foo')
-		end
-
-		it "allows reading for :wheels" do
-			expect(subject.wheels).to eq(4)
-		end
-
-		it "allows writing for doors"   # omitting the do/end block makes this a
-																		# pending example
-
-	end
+	it_behaves_like('a standard vehicle')
 
 	describe ".colors" do		# ruby convention class methods start with a .
 
---
+++ spec/shared_examples/a_standard_vehicle.rb
@@ -0,0 +1,30 @@
+shared_examples_for('a standard vehicle') do
+
+	describe "attributes" do
+
+		it "allows reading and writing for :make" do
+			pending("debugging a problem")
+			subject.make = 'Tes'
+			expect(subject.make).to eq('Test')
+		end
+
+		it "allows reading and writing for :year" do
+			subject.year = 9999
+			expect(subject.year).to eq(9999)
+		end
+
+		it "allows reading and writing for :color" do
+			subject.color = 'foo'
+			expect(subject.color).to eq('foo')
+		end
+
+		it "allows reading for :wheels" do
+			expect(subject.wheels).to eq(4)
+		end
+
+		it "allows writing for doors"   # omitting the do/end block makes this a
+																		# pending example
+
+	end
+
+end
